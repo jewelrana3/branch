c546b2f3235e459fde78dc917e65ef6c99a84fec
jewelrana3 <juyelrana7752@gmail.com>
2025-09-07 15:43:05 +0600

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,3 @@
-import { Header } from "./header/Header";
-import { AppSidebar } from "./components/app-sidebar";
-
-import MainLayout from "./components/layout/MainLayout";
-
-import { SidebarInset, SidebarProvider } from "./components/ui/sidebar";
-
 // import Header from "./pages/header/Header";
 
 function App() {
