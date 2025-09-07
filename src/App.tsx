15f0d63e7c6cb316b129fee72f6341f3d568d865
jewelrana3 <juyelrana7752@gmail.com>
2025-09-07 15:26:55 +0600

    change

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,13 @@
-import { Header } from "./header/Header";
-import { AppSidebar } from "./components/app-sidebar";
-
-import MainLayout from "./components/layout/MainLayout";
-
-import { SidebarInset, SidebarProvider } from "./components/ui/sidebar";
-
 // import Header from "./pages/header/Header";
+
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>
-        {/* header  */}
-        <Header />
-        {/* main content */}
-        <div>
-          <MainLayout />
-        </div>
-      </SidebarInset>
-    </SidebarProvider>
+    <>
+      this app sider
+      <Outlet />
+    </>
   );
 }
 
